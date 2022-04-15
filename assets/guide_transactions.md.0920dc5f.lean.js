8319c9deccc39d9f6cf0482119ee25cbe0dc9305
Sacha STAFYNIAK <sacha@digisquad.io>
2022-04-15 16:21:21 +0200

    deploy

---
+++ assets/guide_transactions.md.0920dc5f.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as s,o as a,a as t}from"./app.d9374415.js";const f='{"title":"Transactions","description":"","frontmatter":{},"headers":[],"relativePath":"guide/transactions.md"}',p={},o=t("",27),c=[o];function e(u,l,i,k,r,d){return a(),s("div",null,c)}var y=n(p,[["render",e]]);export{f as __pageData,y as default};
